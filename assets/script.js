f6331c4a8c7e8d2ca4c29c89bcb05882a7c9cdbf
Dereemii <leshermira@gmail.com>
2020-08-25 14:40:08 -0400

    ejercicio 3

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 // Solicitar al usuario un número entre 1 al 100. Luego, se deberá imprimir por consola los números
 // desde el 0 hasta el número que ingresó el usuario.
 
-    
+    /*
 let inputNumero = prompt("Ingrese un numero entre 1 a 100");
 if( inputNumero > 101 && inputNumero < 0  ){
     alert("Ingresa un numero valido crack")
@@ -19,7 +19,16 @@ if(inputCaballo == "blanco" || inputCaballo == "Blanco" || inputCaballo == "BLAN
     alert("correcto, crack");
 } else{
     alert("vuelve a intentarlo, crack");
-}
+}*/
 
 // Pregunta 3
 
+
+
+
+    var matematicas = prompt("Ingresa tu promedio de matemáticas");
+    var fisica = prompt("Ingresa tu promedio de física");
+    var ciencias = prompt("Ingresa tu promedio de ciencias"); 
+    var promedioNotas = ((parseFloat(matematicas) + parseFloat(ciencias) + parseFloat(fisica)/3));
+    
+    alert("Su promedio es " + promedioNotas);
