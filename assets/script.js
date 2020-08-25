fb8c5a85dfaf502301535585cd0deec9b5c3ea46
Dereemii <leshermira@gmail.com>
2020-08-25 13:58:47 -0400

    pregunta caballo

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,22 @@
 
     
 let inputNumero = prompt("Ingrese un numero entre 1 a 100");
-if( inputNumero > 101 && inputNumero < 0 ){
+if( inputNumero > 101 && inputNumero < 0  ){
     alert("Ingresa un numero valido crack")
-} else{
+} else {
     for(var i = 0; i <= inputNumero; i++){
          console.log(i);
     }
 }
-    
+
+//Pregunta 2
+
+let inputCaballo = prompt("¿De qué color es el caballo blanco de Napoleón?");
+if(inputCaballo == "blanco" || inputCaballo == "Blanco" || inputCaballo == "BLANCO" || inputCaballo == "blanko"){
+    alert("correcto, crack");
+} else{
+    alert("vuelve a intentarlo, crack");
+}
+
+// Pregunta 3
+
